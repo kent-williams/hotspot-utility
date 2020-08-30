128510cf2409c7ba434b4d5b5e487249048f80d4
Kent Williams <k3nt00@gmail.com>
2020-08-30 10:54:31 -0700

    update bluetooth description string

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -6,6 +6,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Hotspot Utility',
       theme: ThemeData(
         primarySwatch: Colors.grey,
